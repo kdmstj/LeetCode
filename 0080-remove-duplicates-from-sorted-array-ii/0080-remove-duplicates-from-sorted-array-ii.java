dde882cf567a4b8ef7b39fbdc4d0558df5b3e6e1
EunSeo Kang <62414231+kdmstj@users.noreply.github.com>
2023-08-24 16:04:16 +0900

    Time: 1 ms (22.07%), Space: 43.6 MB (68.28%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,27 +1,19 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         
-        //1. nums 순회하며 3번 이상 중복이지 않는 숫자 추가
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(nums[0]);
+        int index = 1;
         int count = 1;
-        
         for(int i = 1; i < nums.length; i++){
             if(nums[i] == nums[i-1]){
                 if(++count <= 2){
-                    list.add(nums[i]);
+                    nums[index++] = nums[i];
                 }
             }else{
                 count = 1;
-                list.add(nums[i]);
+                nums[index++] = nums[i];
             }
         }
         
-        //2. list를 nums에 추가
-        for(int i = 0; i < list.size() ; i++){
-            nums[i] = list.get(i);
-        }
-        
-        return list.size();
+        return index;
     }
 }
