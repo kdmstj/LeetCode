9b8eae4976310e05bf73293182b2bae9eeeaa38b
EunSeo Kang <62414231+kdmstj@users.noreply.github.com>
2023-08-24 15:40:25 +0900

    Time: 15 ms (5.01%), Space: 43.8 MB (75.68%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,18 +1,14 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        //1. nums를 순회하면서 list에 중복 없이 원소 추가
         ArrayList<Integer> list = new ArrayList<>();
-        for(int num : nums){
-            if(!list.contains(num)){
-                list.add(num);
+        int filled_index = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            if(!list.contains(nums[i])){
+                list.add(nums[i]);
+                nums[filled_index++] = nums[i];
             }
         }
-        
-        //2. list 순회하며 nums에 추가
-        for(int i= 0; i < list.size() ; i++){
-            nums[i] = list.get(i);
-        }
-        
         return list.size();
     }
 }
