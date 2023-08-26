d49678fe11dbb902972bb83b297b66a5c552cbdb
EunSeo Kang <62414231+kdmstj@users.noreply.github.com>
2023-08-26 15:19:07 +0900

    Time: 2 ms (99.64%), Space: 44 MB (36.79%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,23 +1,21 @@
 class Solution {
     public boolean isPalindrome(String s) {
         //1. 순회하면서
-        int filled_index = 0;
-        char[] charArr = new char[s.length()];
+        ArrayList<Character> list = new ArrayList<>();
         for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
             //1.1 alphanumeric chracters인 경우(소문자/숫자)=> 그대로 대입 + filled_index++;
             if(c >= 97 && c <= 122 || c >= 48 && c <= 57){
-                charArr[filled_index++] = c; 
+                list.add(c);
             }else if(c >= 65 && c <= 90){ //1.2 대문자인 경우 => c + 32 대입 + fille_index++;
-                charArr[filled_index++] = (char)(c + 32);
+                list.add((char)(c + 32));
             }
-            //1.3 non-alphanumeric characters 인경우 => 그냥 지나감
         }
         
         int left = 0;
-        int right = filled_index-1;
+        int right = list.size()-1;
         while(left < right){
-            if(charArr[left] != charArr[right]){
+            if(list.get(left) != list.get(right)){
                 return false;
             }
             left++;
