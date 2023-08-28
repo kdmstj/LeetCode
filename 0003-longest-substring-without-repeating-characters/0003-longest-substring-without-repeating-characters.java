7ef15b413821990faf717f0cb9e453051c63bd84
EunSeo Kang <62414231+kdmstj@users.noreply.github.com>
2023-08-28 15:08:00 +0900

    Time: 5 ms (88.50%), Space: 41.9 MB (97.78%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -2,7 +2,7 @@ class Solution {
     public int lengthOfLongestSubstring(String s) {
         int maxSize = 0;
         int size = 0;
-        int rightIdx = 0; int endIdx = 0;
+        int startIdx = 0; int endIdx = 0;
         
         ArrayList<Character> list = new ArrayList<>();
         //1. s를 순회하면서 list에 추가
@@ -14,10 +14,9 @@ class Solution {
                 //2-1. list 크기 maxSize인지 확인
                 maxSize = Math.max(list.size(), maxSize);
                 //2-2. 포함된 위치까지 제거
-                while(rightIdx < endIdx){
-                    char removed_c = s.charAt(rightIdx);
+                while(startIdx < endIdx){
+                    char removed_c = s.charAt(startIdx++);
                     list.remove(Character.valueOf(removed_c));
-                    rightIdx++;
                     if(removed_c == c){
                         break;
                     }
