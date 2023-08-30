ea2c1a69a17592219169bce714e0d818439381b3
EunSeo Kang <62414231+kdmstj@users.noreply.github.com>
2023-08-30 18:14:37 +0900

    Time: 4 ms (99.20%), Space: 47.1 MB (65.28%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,52 +1,34 @@
 class MinStack {
-    
-    private List<Integer> minList;
-    private int top;
-    private List<Integer> list;
-
-    public MinStack() {
-        this.minList = new ArrayList<>();
-        this.list = new ArrayList<>();
-        this.top = -1;
-    }
-    
-    public void push(int val) {
-        if(!minList.isEmpty() && val <= minList.get(minList.size()-1)){
-            minList.add(val);
-        }else if(minList.isEmpty()){
-            minList.add(val);
-        }
-        list.add(val);
-        top++;
+	private Node head;
+        
+    public void push(int x) {
+        if (head == null) 
+            head = new Node(x, x, null);
+        else 
+            head = new Node(x, Math.min(x, head.min), head);
     }
     
     public void pop() {
-        if(!list.isEmpty()){
-            int element = list.remove(top);
-            top--;
-            if(element == minList.get(minList.size()-1)){
-            minList.remove(minList.size() -1);
-            }
-        }
+        head = head.next;
     }
     
     public int top() {
-        return list.get(top);
+        return head.val;
     }
     
     public int getMin() {
-        if(minList.isEmpty()){
-            throw new RuntimeException("Stack Underflow");
+        return head.min;
+    }
+        
+    private class Node {
+        int val;
+        int min;
+        Node next;
+            
+        private Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
         }
-        return minList.get(minList.size()-1);
     }
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
