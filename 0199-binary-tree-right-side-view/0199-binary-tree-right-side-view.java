a24ce56e01e388099877e6ae7899e20358165f78
EunSeo Kang <62414231+kdmstj@users.noreply.github.com>
2023-09-07 10:11:55 +0900

    Time: 5 ms (5.68%), Space: 41.4 MB (40.80%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        
+        List<Integer> answer = new ArrayList<>();
+        if(root == null){
+            return answer;
+        }
+        
+        Queue<TreeNode> que = new LinkedList<>();
+        que.add(root);
+        while(!que.isEmpty()){
+            Queue<TreeNode> tmp_que = new LinkedList<>();
+            System.out.println(que.peek().val);
+            while(que.size()>=1){
+                TreeNode node = que.poll();
+                
+                if(node.left != null){
+                    tmp_que.add(node.left);
+                }
+                
+                if(node.right != null){
+                    tmp_que.add(node.right);
+                }
+                
+                
+                if(que.size() == 0){
+                    answer.add(node.val);
+                    while(!tmp_que.isEmpty()){
+                        que.add(tmp_que.poll());
+                    }
+                    break;
+                }  
+            }
+           
+        }
+        
+        
+        return answer;
+        
+    }
+}
