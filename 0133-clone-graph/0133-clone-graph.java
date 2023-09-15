759126cc2e346ac1e6e2b456c9822216325c462c
EunSeo Kang <62414231+kdmstj@users.noreply.github.com>
2023-09-15 10:01:44 +0900

    Time: 30 ms (6.67%), Space: 42.2 MB (21.47%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,60 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if(node == null){
+            return null;
+        }
+        
+        Queue<Node[]> que = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+        ArrayList<Node> answer = new ArrayList<>();
+        Node newNode = new Node(node.val);
+        answer.add(newNode);
+        visited.add(newNode.val);
+        que.add(new Node[]{node, newNode});
+        
+        while(!que.isEmpty()){
+            Node[] nodeArr = que.poll();
+            List<Node> neighbors = nodeArr[0].neighbors;
+            for(Node neighbor: neighbors){
+                
+                if(!visited.contains(neighbor.val)){
+                    Node cloneNode = new Node(neighbor.val);
+                    que.add(new Node[]{neighbor, cloneNode});
+                    visited.add(cloneNode.val);
+                    nodeArr[1].neighbors.add(cloneNode);
+                    answer.add(cloneNode);
+                }else{
+                    for(int i = 0; i < answer.size(); i++){
+                        if(neighbor.val == answer.get(i).val){
+                            nodeArr[1].neighbors.add(answer.get(i));
+                        }
+                    }
+                }
+            }
+        }
+        return newNode;
+    }
+    
+    
+        
+}
